b12bf91db32eb74ab3ae0f95175777e0182fd8b0
Hack Reactor Students <accounts@hackreactor.com>
2015-09-16 15:45:35 -0700

    changed port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var app = require('./server-config.js');
 
 var port = process.env.PORT || 4568;
 
-app.listen(process.env.PORT);
+app.listen(port);
 
 console.log('Server now listening on port ' + port);
